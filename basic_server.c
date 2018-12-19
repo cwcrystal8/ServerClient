5c92ffc7b9fd2519cb51f3bf6c7d339d0c2a93d5
Crystal Wang <cwang4@stuy.edu>
2018-12-19 10:40:25 -0500

    worked

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,29 @@
 #include "pipe_networking.h"
 
+void process_data(char *data){
+  while(*data){
+    *data = 26 - (*data) + 2 * 'a';
+    data++;
+  }
+}
+
 
 int main() {
 
   int to_client;
   int from_client;
 
-  from_client = server_handshake( &to_client );
+  while(1){
+    from_client = server_handshake( &to_client );
+    
+  }
+
 }
+
+/*
+get data from client
+process the data
+reply to client
+when client exits, reset
+
+ */
--- client.c
+++ client.c
@@ -1,10 +1,23 @@
 #include "pipe_networking.h"
 
-
 int main() {
 
   int to_server;
   int from_server;
 
   from_server = client_handshake( &to_server );
+  char input[BUFFER_SIZE], output[BUFFER_SIZE];
+  while(1){
+    printf("[Client] Enter data: ");
+    fgets(input, BUFFER_SIZE, stdin);
+    write(to_server, input, BUFFER_SIZE);
+    while(!read(from_server, output, BUFFER_SIZE));
+    printf("[Client] New and improved data: %s\n", output);  
+  }
 }
+
+/*
+gets input from stdin
+gets response from server
+displays response
+ */
